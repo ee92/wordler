0088754d167fdc77b85d4ad623cb9e7b4f002a6d
Egor Egorov <650egor@gmail.com>
2022-09-11 20:15:05 -0700

    add animation for input validation

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,7 @@ const Home: NextPage = () => {
 	const [yellows, setYellows] = useState<Letter[][]>(initArray(LETTERS_IN_WORD, [defaultLetter]));
 	const [grey, setGreys] = useState("");
 	const [results, setResults] = useState<string[]>([]);
+	const [invalid, setInvalid] = useState<string>();
 
 	function focusEl(el: HTMLElement | null) {
 		el && el.focus();
@@ -42,6 +43,19 @@ const Home: NextPage = () => {
 		});
 	}, [yellows]);
 
+	// remove animation so it can run again when added the next time, also lame.
+	useEffect(() => {
+		let timeout: ReturnType<typeof setTimeout>;
+		if (invalid) {
+			timeout = setTimeout(() => {
+				setInvalid(undefined);
+			}, 300) // animation duration
+		}
+		return () => {
+			timeout && clearTimeout(timeout);
+		}
+	}, [invalid]);
+
 	return (
 		<div className="flex justify-center">
 			<div>
@@ -51,7 +65,7 @@ const Home: NextPage = () => {
 						{greens.map((letter, i) => (
 							<input
 								key={i}
-								className={`w-10 h-10 rounded text-center ${letter.value ? "bg-green-300" : "bg-green-200"}`}
+								className={`w-10 h-10 rounded text-center ${invalid === `green-${i}` ? "animate-shake" : ""} ${letter.value ? "bg-green-300" : "bg-green-200"}`}
 								value={letter.value}
 								onKeyDown={(e: any) => {
 									if (e.key === "Backspace") {
@@ -65,8 +79,12 @@ const Home: NextPage = () => {
 									}
 								}}
 								onChange={(e: any) => {
+									if (/^([^A-Za-z]*)$/.test(e.target.value)) {
+										setInvalid(`green-${i}`);
+										return;
+									}
 									const input = e.target.value.replace(/[^A-Za-z]/ig, '');
-									const letter = input && input.charAt(input.length - 1).toUpperCase();
+									const letter = input && input[input.length - 1].toUpperCase();
 									if (letter && !greens.some(g => g.value === letter)) {
 										const updatedLetter = { value: letter };
 										setGreens([...greens.slice(0, i), updatedLetter, ...greens.slice(i + 1)]);
@@ -91,16 +109,17 @@ const Home: NextPage = () => {
 						{yellows.map((slot, i) => (
 							<div key={i} id={`yellow-${i}`} className="flex flex-col gap-1">
 								{slot.map((letter, j) => {
+									const id = `yellow-${i}-${j}`;
 									const maxLetters = LETTERS_IN_WORD - 1; // if you have all letter you won 😁
 									const isLastLetterInSlot = j + 1 === maxLetters;
 									const isSlotFull = j + 1 > maxLetters;
 									if (isSlotFull) return null;
 									return (
 										<input
-											key={`yellow-${i}-${j}`}
-											id={`yellow-${i}-${j}`}
+											key={id}
+											id={id}
 											autoFocus={letter.autoFocus}
-											className={`w-10 h-10 rounded text-center ${letter.value ? "bg-yellow-300" : "bg-yellow-200"}`}
+											className={`w-10 h-10 rounded text-center ${invalid === id ? "animate-shake" : ""} ${letter.value ? "bg-yellow-300" : "bg-yellow-200"}`}
 											value={letter.value}
 											onKeyDown={(e: any) => {
 												if (e.key === "Backspace" && letter) {
@@ -143,6 +162,10 @@ const Home: NextPage = () => {
 												}
 											}}
 											onChange={(e: any) => {
+												if (/^([^A-Za-z]*)$/.test(e.target.value)) {
+													setInvalid(id);
+													return;
+												}
 												const input = e.target.value.replace(/[^A-Za-z]/ig, '');
 												const letter = input && input.charAt(input.length - 1).toUpperCase();
 												const letterNotInSlot = letter && !slot.some(l => l.value === letter);
@@ -150,7 +173,7 @@ const Home: NextPage = () => {
 													let updatedLetter = { value: letter };
 													let updatedSlot = [...slot.slice(0, j), updatedLetter, ...slot.slice(j + 1)];
 													setYellows([...yellows.slice(0, i), updatedSlot, ...yellows.slice(i + 1)]);
-													// focus next input in slot if possible (not possible for last slot in slot)
+													// focus next input in slot if possible (not possible for last letter in slot)
 													// auto focus attribute handles focus when the additional inputs mount
 													focusEl(document.getElementById(`yellow-${i}-${j + 1}`));
 													if (isLastLetterInSlot) {
@@ -176,9 +199,16 @@ const Home: NextPage = () => {
 				<div>Greys</div>
 				<div className="flex">
 					<input
-						className={`w-[13.5rem] h-10 rounded text-center ${grey ? "bg-gray-200" : "bg-gray-100"}`}
+						className={`w-[13.5rem] h-10 rounded text-center ${invalid === "grey" ? "animate-shake" : ""} ${grey ? "bg-gray-200" : "bg-gray-100"}`}
 						value={grey}
-						onChange={(e) => setGreys(e.target.value.replace(/[^A-Za-z]/ig, '').toUpperCase())}
+						onChange={(e) => {
+							const input = e.target.value;
+							if (input && !/^[a-zA-Z]+$/.test(input)) {
+								setInvalid("grey");
+								return;
+							}
+							setGreys(input.replace(/[^A-Za-z]/ig, '').toUpperCase());
+						}}
 					/>
 					<button
 						className='w-6 h-6 m-2 rounded-full border-2 border-blue-600 text-blue-600 flex justify-center items-center outline-offset-2'
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,24 @@ module.exports = {
     "./ui/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        shake: 'shake 300ms'
+      },
+      keyframes: {
+        shake: {
+          '25%': {
+            transform: 'translateX(2px)'
+          },
+          '50%': {
+            transform: 'translateX(-2px)'
+          },
+          '75%': {
+            transform: 'translateX(2px)'
+          }
+        }
+      }
+    },
   },
   plugins: [],
 }
